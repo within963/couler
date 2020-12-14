d37ad711859ec5707cdd86ce3d79c71c3bbadf24
terrytangyuan <terrytangyuan@gmail.com>
2020-12-14 15:25:50 -0500

    Add back watch
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- go/couler/submitter/argo_submitter.go
+++ go/couler/submitter/argo_submitter.go
@@ -2,6 +2,8 @@ package submitter
 
 import (
 	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/tools/clientcmd"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
@@ -32,25 +34,25 @@ func (submitter *ArgoWorkflowSubmitter) Submit(wf wfv1.Workflow) (wfv1.Workflow,
 	}
 	fmt.Printf("Workflow %s successfully submitted\n", createdWf.Name)
 
-	//// Wait for the workflow to complete
-	//fieldSelector := fields.ParseSelectorOrDie(fmt.Sprintf("metadata.name=%s", createdWf.Name))
-	//watchIf, err := wfClient.Watch(metav1.ListOptions{FieldSelector: fieldSelector.String()})
-	//if err != nil {
-	//	return wf, fmt.Errorf("failed establish a watch")
-	//}
-	//defer watchIf.Stop()
-	//for next := range watchIf.ResultChan() {
-	//	wf, ok := next.Object.(*wfv1.Workflow)
-	//	if !ok {
-	//		continue
-	//	}
-	//	if wf.Status.Phase == wfv1.NodeFailed || wf.Status.Phase == wfv1.NodeError {
-	//		return *wf, fmt.Errorf("workflow %s failed: %s", wf.Name, wf.Status.Message)
-	//	}
-	//	if !wf.Status.FinishedAt.IsZero() {
-	//		fmt.Printf("Workflow %s %s at %v\n", wf.Name, wf.Status.Phase, wf.Status.FinishedAt)
-	//		break
-	//	}
-	//}
+	// Wait for the workflow to complete
+	fieldSelector := fields.ParseSelectorOrDie(fmt.Sprintf("metadata.name=%s", createdWf.Name))
+	watchIf, err := wfClient.Watch(metav1.ListOptions{FieldSelector: fieldSelector.String()})
+	if err != nil {
+		return wf, fmt.Errorf("failed establish a watch")
+	}
+	defer watchIf.Stop()
+	for next := range watchIf.ResultChan() {
+		wf, ok := next.Object.(*wfv1.Workflow)
+		if !ok {
+			continue
+		}
+		if wf.Status.Phase == wfv1.NodeFailed || wf.Status.Phase == wfv1.NodeError {
+			return *wf, fmt.Errorf("workflow %s failed: %s", wf.Name, wf.Status.Message)
+		}
+		if !wf.Status.FinishedAt.IsZero() {
+			fmt.Printf("Workflow %s %s at %v\n", wf.Name, wf.Status.Phase, wf.Status.FinishedAt)
+			break
+		}
+	}
 	return wf, nil
 }
